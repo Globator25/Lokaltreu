6158524317d83b373adab1314f0edb5a2d126c39
Globator25 <recouvr@gmail.com>
2025-10-25 12:02:24 +0200

    IaC: align postgres module inputs in prod to actual module interface

--- infra/terraform/envs/prod/main.tf
+++ infra/terraform/envs/prod/main.tf
@@ -24,73 +24,42 @@ terraform {
   }
 }
 
-module "app" {
-  source = "../../modules/app"
-  project       = var.project
-  environment   = var.environment
-  naming_prefix = var.naming_prefix
-  region        = var.region_api
-  fly_org_slug  = var.fly_org_slug
-  fly_app_name  = var.fly_app_name
-  tags          = merge(var.tags, { tier = "prod" })
-}
+module "platform" {
+  source = "../.."
 
-module "postgres" {
-  source           = "../../modules/postgres"
-  project          = var.project
-  environment      = var.environment
-  naming_prefix    = var.naming_prefix
-  region           = var.region_postgres
-  neon_project_id  = var.neon_project_id
-  neon_branch_name = var.neon_branch_name
-  neon_api_key     = var.neon_api_key
-  database_name    = var.database_name
-  tags             = merge(var.tags, { tier = "prod" })
-}
+  config = {
+    project               = var.project
+    environment           = var.environment
+    naming_prefix         = var.naming_prefix
 
-module "redis" {
-  source          = "../../modules/redis"
-  project         = var.project
-  environment     = var.environment
-  naming_prefix   = var.naming_prefix
-  region          = var.region_redis
-  upstash_team_id = var.upstash_team_id
-  datastore_name   = var.datastore_name
-  upstash_email    = var.upstash_email
-  upstash_api_key  = var.upstash_api_key
-  tags             = merge(var.tags, { tier = "prod" })
-}
+    region_api            = var.region_api
+    region_postgres       = var.region_postgres
+    region_redis          = var.region_redis
+    region_storage        = var.region_storage
+    region_cdn            = var.region_cdn
 
-module "cdn" {
-  source                = "../../modules/cdn"
-  project               = var.project
-  environment           = var.environment
-  naming_prefix         = var.naming_prefix
-  region                = var.region_cdn
-  cloudflare_account_id = var.cloudflare_account_id
-  cloudflare_api_token  = var.cloudflare_api_token
-  zone_label            = var.zone_label
-  cname                 = var.cname
-  tags                  = merge(var.tags, { tier = "prod" })
-}
+    fly_org_slug          = var.fly_org_slug
+    fly_app_name          = var.fly_app_name
+    fly_access_token      = var.fly_access_token
 
-module "mail" {
-  source        = "../../modules/mail"
-  project       = var.project
-  environment   = var.environment
-  naming_prefix = var.naming_prefix
-  mail_provider = var.mail_provider
-  mail_api_key  = var.mail_api_key
-  mail_service  = var.mail_service
-  tags          = merge(var.tags, { tier = "prod" })
-}
+    neon_project_id       = var.neon_project_id
+    neon_branch_name      = var.neon_branch_name
+    neon_api_key          = var.neon_api_key
 
-module "storage" {
-  source                = "../../modules/storage"
-  project               = var.project
-  environment           = var.environment
-  naming_prefix         = var.naming_prefix
-  region                = var.region_storage
-  cloudflare_account_id = var.cloudflare_account_id
-  tags                  = merge(var.tags, { tier = "prod" })
+    upstash_team_id       = var.upstash_team_id
+    upstash_email         = var.upstash_email
+    upstash_api_key       = var.upstash_api_key
+
+    cloudflare_account_id = var.cloudflare_account_id
+    cloudflare_api_token  = var.cloudflare_api_token
+
+    zone_label            = var.zone_label
+    cname                 = var.cname
+
+    mail_provider         = var.mail_provider
+    mail_service          = var.mail_service
+    mail_api_key          = var.mail_api_key
+
+    tags                  = merge(var.tags, { tier = "prod" })
+  }
 }

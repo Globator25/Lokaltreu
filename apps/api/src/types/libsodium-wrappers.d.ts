a07a998542ef090cf7053358dcdb6a04ce50e29c
Globator25 <recouvr@gmail.com>
2026-01-08 20:10:11 +0100

    chore: align packages for step22 plan enforcement

--- apps/api/src/types/libsodium-wrappers.d.ts
+++ apps/api/src/types/libsodium-wrappers.d.ts
@@ -24,3 +24,30 @@ declare module "libsodium-wrappers" {
   const sodium: Sodium;
   export default sodium;
 }
+
+declare module "libsodium-wrappers/dist/modules/libsodium-wrappers.js" {
+  type SodiumKeyPair = {
+    publicKey: Uint8Array;
+    privateKey: Uint8Array;
+  };
+
+  type Sodium = {
+    ready: Promise<void>;
+    base64_variants: {
+      ORIGINAL: number;
+    };
+    from_base64: (input: string, variant: number) => Uint8Array;
+    from_string: (input: string) => Uint8Array;
+    to_base64: (input: Uint8Array, variant: number) => string;
+    crypto_sign_keypair: () => SodiumKeyPair;
+    crypto_sign_detached: (message: Uint8Array, privateKey: Uint8Array) => Uint8Array;
+    crypto_sign_verify_detached: (
+      signature: Uint8Array,
+      message: Uint8Array,
+      publicKey: Uint8Array,
+    ) => boolean;
+  };
+
+  const sodium: Sodium;
+  export default sodium;
+}
